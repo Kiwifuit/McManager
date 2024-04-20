7c463b4ae03f25c3a9ec652a0b0f35a6b9bf874c
Kiwifuit <mahkiwi123@gmail.com>
2024-04-20 23:01:37 +0800

    impl: api checker

---
+++ modrinth/src/api.rs
@@ -0,0 +1,31 @@
+use std::time::Duration;
+
+use reqwest::Client;
+use thiserror::Error;
+
+const ENDPOINT: &str = "https://api.modrinth.com";
+
+#[derive(Debug, Error)]
+pub enum APIError {
+    #[error("http error: {0}")]
+    Http(#[from] reqwest::Error),
+}
+
+pub async fn check_api() -> Result<(bool, Client), APIError> {
+    let client = Client::builder()
+        .user_agent(format!(
+            "{} using {} v{}",
+            std::env::var("CARGO_BIN_NAME").unwrap_or(String::from("<unknown>")),
+            env!("CARGO_PKG_NAME"),
+            env!("CARGO_PKG_VERSION")
+        ))
+        .https_only(true)
+        .timeout(Duration::from_secs(30))
+        .connection_verbose(false)
+        .redirect(reqwest::redirect::Policy::none())
+        .build()?;
+
+    let resp = client.get(ENDPOINT).send().await;
+
+    Ok((resp.is_ok(), client))
+}
