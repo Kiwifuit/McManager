dd41490e8f1e2ac24c182eb7f9b04c073b21d773
Kiwifuit <mahkiwi123@gmail.com>
2024-07-04 18:56:16 +0800

    impl: modrinth modpack manifest

---
+++ mparse/src/types/modrinth.rs
@@ -0,0 +1,33 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ModrinthModpack {
+    pub game: String,
+    pub version_id: String,
+    pub name: String,
+    pub summary: Option<String>,
+    pub files: Vec<ModrinthModpackFiles>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ModrinthModpackFiles {
+    pub path: String,
+    pub hashes: ModpackFileHashes,
+    pub env: ModpackEnv,
+    pub downloads: Vec<String>,
+    pub file_size: usize,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ModpackFileHashes {
+    pub sha1: String,
+    pub sha2: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ModpackEnv {
+    pub server: String,
+    pub client: String,
+}
