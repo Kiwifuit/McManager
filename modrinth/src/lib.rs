d059ba303329b5458c65fc8950da21005364d444
Kiwifuit <mahkiwi123@gmail.com>
2024-07-04 19:40:57 +0800

    impl: decoupled modrith's `types` module for ez acces 😎

--- modrinth/src/lib.rs
+++ modrinth/src/lib.rs
@@ -1,9 +1,15 @@
-mod api;
-mod types;
+pub mod api;
+#[cfg(feature = "types")]
+pub mod types;
 
 pub use api::*;
+#[cfg(feature = "types")]
 pub use types::project::ModrinthProject;
+#[cfg(feature = "types")]
 pub use types::query::{ProjectQueryBuilder, VersionQueryBuilder};
+#[cfg(feature = "types")]
 pub use types::version::ModrinthProjectVersion;
+#[cfg(feature = "types")]
 pub use types::{Facet, FacetOp};
+#[cfg(feature = "types")]
 pub use types::{IndexBy, Loader};
