5e908e666d3e80f4ea0e35df2078acbde8f28abb
Kiwifuit <mahkiwi123@gmail.com>
2024-07-15 15:05:38 +0800

    impl: `Deserialize` for manifest

--- mpack/src/model.rs
+++ mpack/src/model.rs
@@ -1,22 +1,22 @@
 use std::path::PathBuf;
 
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use sha1_smol::Digest;
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct GenericModpackManifest {
     version_commit: String,
     files: Vec<GenericModpackFile>,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct GenericModpackFile {
     file_type: GenericModpackFileType,
     path: PathBuf,
     hash: Digest,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
 enum GenericModpackFileType {
     Mod,
