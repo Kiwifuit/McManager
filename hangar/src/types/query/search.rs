b4885ade5a4f52244690ab3a391d0827c8741ee5
Kiwifuit <mahkiwi123@gmail.com>
2024-07-22 15:20:49 +0800

    impl: `VersionQuery` and `SearchQuery` builder structs

--- hangar/src/types/query/search.rs
+++ hangar/src/types/query/search.rs
@@ -2,16 +2,16 @@ use super::GenericPagination;
 use crate::types::{HangarPlatform, HangarTags};
 
 pub struct SearchQuery {
-    prioritize_exact_match: bool,
-    pagination: GenericPagination,
-    sort: SortBy,
-    category: String,
-    platform: HangarPlatform,
-    owner: String,
-    query: String,
-    license: String,
-    version: String,
-    tag: HangarTags,
+    pub(crate) prioritize_exact_match: bool,
+    pub(crate) pagination: GenericPagination,
+    pub(crate) sort: SortBy,
+    pub(crate) category: String,
+    pub(crate) platform: HangarPlatform,
+    pub(crate) owner: String,
+    pub(crate) query: String,
+    pub(crate) license: String,
+    pub(crate) version: String,
+    pub(crate) tag: HangarTags,
 }
 
 pub enum SortBy {
@@ -23,4 +23,94 @@ pub enum SortBy {
     RecentDownloads,
     RecentViews,
     Slugs,
+}
+
+pub struct SearchQueryBuilder {
+    prioritize_exact_match: Option<bool>,
+    pagination: Option<GenericPagination>,
+    sort: Option<SortBy>,
+    category: Option<String>,
+    platform: Option<HangarPlatform>,
+    owner: Option<String>,
+    query: Option<String>,
+    license: Option<String>,
+    version: Option<String>,
+    tag: Option<HangarTags>,
+}
+
+impl SearchQueryBuilder {
+    pub fn prioritize_exact_match(mut self, prioritize_exact_match: bool) -> Self {
+        self.prioritize_exact_match = Some(prioritize_exact_match);
+
+        self
+    }
+
+    pub fn pagination(mut self, pagination: GenericPagination) -> Self {
+        self.pagination = Some(pagination);
+
+        self
+    }
+
+    pub fn sort(mut self, sort: SortBy) -> Self {
+        self.sort = Some(sort);
+
+        self
+    }
+
+    pub fn category(mut self, category: String) -> Self {
+        self.category = Some(category);
+
+        self
+    }
+
+    pub fn platform(mut self, platform: HangarPlatform) -> Self {
+        self.platform = Some(platform);
+
+        self
+    }
+
+    pub fn owner(mut self, owner: String) -> Self {
+        self.owner = Some(owner);
+
+        self
+    }
+
+    pub fn query(mut self, query: String) -> Self {
+        self.query = Some(query);
+
+        self
+    }
+
+    pub fn license(mut self, license: String) -> Self {
+        self.license = Some(license);
+
+        self
+    }
+
+    pub fn version(mut self, version: String) -> Self {
+        self.version = Some(version);
+
+        self
+    }
+
+    pub fn tag(mut self, tag: HangarTags) -> Self {
+        self.tag = Some(tag);
+
+        self
+    }
+
+    pub fn build(self) -> SearchQuery {
+        SearchQuery {
+            prioritize_exact_match: self.prioritize_exact_match.unwrap(),
+            pagination: self.pagination.unwrap(),
+            sort: self.sort.unwrap(),
+            category: self.category.unwrap(),
+            platform: self.platform.unwrap(),
+            owner: self.owner.unwrap(),
+            query: self.query.unwrap(),
+            license: self.license.unwrap(),
+            version: self.version.unwrap(),
+            tag: self.tag.unwrap(),
+        }
+    }
 }
--- hangar/src/types/query/version.rs
+++ hangar/src/types/query/version.rs
@@ -7,3 +7,46 @@ pub struct VersionQuery {
     pub(crate) platform: HangarPlatform,
     pub(crate) platform_version: String,
 }
+
+#[derive(Debug, Default)]
+pub struct VersionQueryBuilder {
+    pagination: Option<GenericPagination>,
+    include_hidden_channels: Option<bool>,
+    platform: Option<HangarPlatform>,
+    platform_version: Option<String>,
+}
+
+impl VersionQueryBuilder {
+    pub fn pagination(mut self, pagination: GenericPagination) -> Self {
+        self.pagination = Some(pagination);
+
+        self
+    }
+
+    pub fn include_hidden_channels(mut self, include_hidden_channels: bool) -> Self {
+        self.include_hidden_channels = Some(include_hidden_channels);
+
+        self
+    }
+
+    pub fn platform(mut self, platform: HangarPlatform) -> Self {
+        self.platform = Some(platform);
+
+        self
+    }
+
+    pub fn platform_version(mut self, platform_version: String) -> Self {
+        self.platform_version = Some(platform_version);
+
+        self
+    }
+
+    pub fn build(self) -> VersionQuery {
+        VersionQuery {
+            include_hidden_channels: self.include_hidden_channels.unwrap(),
+            pagination: self.pagination.unwrap(),
+            platform: self.platform.unwrap(),
+            platform_version: self.platform_version.unwrap(),
+        }
+    }
+}
