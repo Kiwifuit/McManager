dafbcce4a4793a4821fdbc55a17bfe2c37f272bc
Kiwifuit <mahkiwi123@gmail.com>
2024-07-27 21:26:44 +0800

    feat: `get_version` and checking if a version is set in `get_versions`

--- mar/src/repository.rs
+++ mar/src/repository.rs
@@ -7,7 +7,10 @@ use thiserror::Error;
 #[derive(Debug, Error)]
 pub enum RepositoryError {
     #[error("version already available")]
-    RepositoryAvailable,
+    VersionAvailable,
+
+    #[error("version not available")]
+    NoVersion,
 
     #[error("http error: {0}")]
     Http(#[from] reqwest::Error),
@@ -16,23 +19,46 @@ pub enum RepositoryError {
     XmlParse(#[from] quick_xml::DeError),
 }
 
-pub async fn get_versions(
+pub async fn get_versions<T: ToString>(
     artifact: &MavenArtifact,
-    base_url: String,
+    base_url: T,
 ) -> Result<MavenArtifactVersions, RepositoryError> {
-    let raw = get(format!(
-        "{}/{}/{}/maven-metadata.xml",
-        base_url,
-        artifact.group_id.replace('.', "/"),
-        artifact.artifact_id
-    ))
-    .await?
-    .text()
-    .await?;
+    if artifact.version.is_some() {
+        Err(RepositoryError::VersionAvailable)
+    } else {
+        let raw = get(format!(
+            "{}/{}/{}/maven-metadata.xml",
+            base_url.to_string(),
+            artifact.group_id.replace('.', "/"),
+            artifact.artifact_id
+        ))
+        .await?
+        .text()
+        .await?;
 
-    let parsed = from_str(&raw)?;
+        let parsed = from_str(&raw)?;
 
-    Ok(parsed)
+        Ok(parsed)
+    }
+}
+
+pub fn get_artifact<T: ToString>(
+    artifact_data: &MavenArtifact,
+    base_url: T,
+    artifact_name: T,
+) -> Result<String, RepositoryError> {
+    if artifact_data.version.is_none() {
+        Err(RepositoryError::NoVersion)
+    } else {
+        Ok(format!(
+            "{}/{}/{}/{}/{}",
+            base_url.to_string(),
+            artifact_data.group_id.replace('.', "/"),
+            artifact_data.artifact_id,
+            artifact_data.version.clone().unwrap(),
+            artifact_name.to_string()
+        ))
+    }
 }
 
 #[cfg(test)]
@@ -47,5 +73,29 @@ mod tests {
             get_versions(&artifact, "https://maven.minecraftforge.net".to_string()).await;
 
         assert!(versions.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_get_version() {
+        let mut artifact = MavenArtifact::new("forge", "net.minecraftforge");
+        let base_url = "https://maven.minecraftforge.net".to_string();
+
+        let selected_version = get_versions(&artifact, &base_url)
+            .await
+            .unwrap()
+            .versioning
+            .latest();
+
+        artifact.set_version(&selected_version);
+        let artifact_name = format!("forge-{}-installer.jar", selected_version);
+        let expected_artifact_url = format!(
+            "https://maven.minecraftforge.net/net/minecraftforge/forge/{0}/forge-{0}-installer.jar",
+            selected_version
+        );
+        let artifact_url = get_artifact(&artifact, base_url, artifact_name);
+
+        assert!(artifact_url.is_ok());
+        let artifact_url = artifact_url.unwrap();
+        assert_eq!(artifact_url, expected_artifact_url);
     }
 }
