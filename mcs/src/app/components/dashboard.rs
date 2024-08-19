0cf0b46175a8944d78ba05e33fb06dda0de72baf
Kiwifuit <mahkiwi123@gmail.com>
2024-08-19 20:07:19 +0800

    impl: `OwnedServers` component

---
+++ mcs/src/app/components/dashboard.rs
@@ -0,0 +1,18 @@
+use leptos::*;
+
+#[component]
+fn OwnedServers(server_list: Vec<(String, String)>) -> impl IntoView {
+    view! {
+        <ul>
+            {
+                server_list.iter().map(|(id, name)| {
+                    view! {
+                        <li>
+                            <a href={format!("/servers/{}", id)}>{name}</a>
+                        </li>
+                    }
+                }).collect::<Vec<_>>()
+            }
+        </ul>
+    }
+}
