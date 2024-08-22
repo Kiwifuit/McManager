10e6026a01d6e153f99d42931d04cd7d34d40132
Kiwifuit <mahkiwi123@gmail.com>
2024-08-22 17:34:27 +0800

    feat: `modrinth` crate

--- modrinth/src/types/result.rs
+++ modrinth/src/types/result.rs
@@ -1,9 +1,10 @@
 use super::{Gallery, License, ModRequirement, ModrinthProjectMeta, ProjectType};
 use serde::Deserialize;
+use std::rc::Rc;
 
 #[derive(Debug, Deserialize)]
 pub struct SearchProjectResult {
-    pub hits: Vec<SearchProjectHit>,
+    pub hits: Rc<[SearchProjectHit]>,
     pub offset: u8,
     pub limit: u8,
     pub total_hits: u16,
@@ -16,13 +17,13 @@ pub struct SearchProjectResult {
 /// *been copied over from [Modrinth's documentation](https://docs.modrinth.com/#tag/project_result_model)*
 pub struct SearchProjectHit {
     /// The slug of a project, used for vanity URLs. Regex: `^[\w!@$()`.+,"\-']{3,64}$`
-    pub slug: String,
+    pub slug: Rc<str>,
     /// The title or name of the project
-    pub title: String,
+    pub title: Rc<str>,
     /// A short description of the project
-    pub description: String,
+    pub description: Rc<str>,
     /// A list of the categories that the project has
-    pub categories: Vec<String>,
+    pub categories: Vec<Rc<str>>,
     /// The client side support of the project
     pub client_side: ModRequirement,
     /// The server side support of the project
@@ -32,37 +33,41 @@ pub struct SearchProjectHit {
     /// The total number of downloads of the project
     pub downloads: u32,
     /// The URL of the project's icon
-    pub icon_url: String,
+    pub icon_url: Rc<str>,
     /// The RGB color of the project, automatically generated from the project icon
     pub color: u32,
     /// The ID of the project
-    pub project_id: String,
+    pub project_id: Rc<str>,
     /// The username of the project's author
-    pub author: String,
+    pub author: Rc<str>,
     /// A list of the minecraft versions supported by the project
-    pub versions: Vec<String>,
+    pub versions: Vec<Rc<str>>,
     /// The date the project was added to search
-    pub date_created: String,
+    pub date_created: Rc<str>,
     /// The date the project was last modified
-    pub date_modified: String,
+    pub date_modified: Rc<str>,
     /// The latest version of minecraft that this project supports
-    pub latest_version: String,
+    pub latest_version: Rc<str>,
     /// The SPDX license ID of a project
     pub license: License,
     /// All gallery images attached to the project
     pub gallery: Gallery,
     /// The featured gallery image of the project
-    pub featured_gallery: Option<String>,
+    pub featured_gallery: Option<Rc<str>>,
 }
 
 impl ModrinthProjectMeta for SearchProjectHit {
-    fn project_id(&self) -> Option<&String> {
-        Some(&self.project_id)
+    type Id = Rc<str>;
+
+    fn project_id(&self) -> Option<Self::Id> {
+        Some(self.project_id.clone())
     }
 }
 
 impl ModrinthProjectMeta for &SearchProjectHit {
-    fn project_id(&self) -> Option<&String> {
-        Some(&self.project_id)
+    type Id = Rc<str>;
+
+    fn project_id(&self) -> Option<Self::Id> {
+        Some(self.project_id.clone())
     }
 }
