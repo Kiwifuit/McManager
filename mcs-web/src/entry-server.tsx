f07d892f4d36aacd8b0eb120e86bdc876165edcd
Kiwifuit <mahkiwi123@gmail.com>
2024-08-26 13:14:22 +0800

    feat: homepage

--- mcs-web/src/entry-server.tsx
+++ mcs-web/src/entry-server.tsx
@@ -1,5 +1,7 @@
 // @refresh reload
 import { createHandler, StartServer } from "@solidjs/start/server";
+import { BsGithub } from 'solid-icons/bs';
+import { FaSolidCode } from 'solid-icons/fa';
 
 export default createHandler(() => (
   <StartServer
@@ -13,11 +15,19 @@ export default createHandler(() => (
         </head>
         <body class="dark:bg-dark-bg dark:text-dark-fg">
           <div id="app" >{children}</div>
-          <footer class="mt-2">
+          <footer class="mt-2 text-x">
             <ul class="flex justify-center content-center italic gap-x-1">
               <li>MCS by Kiwifuit</li>
-              <li><a href="https://github.com/Kiwifuit" target="_blank" rel="noopener noreferrer">Github</a></li>
-              <li><a href="https://github.com/Kiwifuit/McManager" target="_blank" rel="noopener noreferrer">Repository</a></li>
+              <li class="flex items-center ml-1" >
+                <a href="https://github.com/Kiwifuit" target="_blank" rel="noopener noreferrer">
+                  <BsGithub />
+                </a>
+              </li>
+              <li class="flex items-center ml-1 top-10" >
+                <a href="https://github.com/Kiwifuit/McManager" target="_blank" rel="noopener noreferrer">
+                  <FaSolidCode />
+                </a>
+              </li>
             </ul>
           </footer>
           {scripts}
--- mcs-web/src/routes/index.tsx
+++ mcs-web/src/routes/index.tsx
@@ -2,9 +2,14 @@ import { Title } from "@solidjs/meta";
 
 export default function Home() {
   return (
-    <main>
+    <main class="grid place-items-center w-screen mt-32">
       <Title>Servers</Title>
-
+      <div class="w-1/2">
+        <h1 class="text-4xl font-bold mb-2">Servers:</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex ipsa molestiae impedit explicabo accusamus inventore adipisci reiciendis iste dolor, nobis earum consequatur quod commodi quis fuga quas, assumenda, rerum ipsam.</p>
+        <p>Repellendus itaque culpa consequuntur quas ratione minus vel repudiandae ea? At architecto nisi vero rerum, consequuntur deleniti dolorem quasi rem iste, explicabo facere, facilis doloribus esse perferendis temporibus distinctio hic.</p>
+        <p>Sequi fuga, aperiam expedita dicta ipsam ut minus ex, iste sint, iure autem? Molestiae dolorem nulla error ex, voluptates molestias quas itaque temporibus harum voluptatem provident, libero quam deleniti nesciunt.</p>
+      </div>
     </main>
   );
 }
