9f2ca1008a4908bd17f72b5c71c18f56053a3da1
Kiwifuit <mahkiwi123@gmail.com>
2024-08-26 10:51:33 +0800

    feat: added icons to the navbar

--- mcs-web/src/app.tsx
+++ mcs-web/src/app.tsx
@@ -1,6 +1,7 @@
 import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
+import { BiSolidCog, BiSolidHomeAlt2, BiSolidInfoCircle, BiSolidServer } from 'solid-icons/bi';
 import { Suspense } from "solid-js";
 import "./app.css";
 
@@ -10,12 +11,24 @@ export default function App() {
       root={props => (
         <MetaProvider>
           <Title>MCS</Title>
-          <nav>
+          <nav class="mx-3 my-1">
             <ul class="flex gap-4 text-xl">
-              <li><a href="/">Home</a></li>
-              <li class="grow"><a href="/servers">Servers</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/options">MCS Options</a></li>
+              <li class="flex items-center">
+                <BiSolidHomeAlt2 />
+                <a href="/">Home</a>
+              </li>
+              <li class="grow flex items-center">
+                <BiSolidServer />
+                <a href="/servers">Servers</a>
+              </li>
+              <li class="flex items-center">
+                <BiSolidInfoCircle />
+                <a href="/about">About</a>
+              </li>
+              <li class="flex items-center">
+                <BiSolidCog />
+                <a href="/options">MCS Options</a>
+              </li>
             </ul>
           </nav>
           <Suspense>{props.children}</Suspense>
