6b3144e16d8eefdb8ae7d0e4bd5a0de715bb0e4c
Kiwifuit <mahkiwi123@gmail.com>
2024-09-05 07:08:34 +0800

    chore: fixed issue with `libmcs_version` symbol not showing

--- mcs-ffi/src/lib.rs
+++ mcs-ffi/src/lib.rs
@@ -1,5 +1,6 @@
 use std::ffi::{c_char, CString};
 
+#[no_mangle]
 pub extern "C" fn libmcs_version() -> *const c_char {
     CString::new(format!("mcs backend v{}", env!("CARGO_PKG_VERSION")))
         .unwrap()
