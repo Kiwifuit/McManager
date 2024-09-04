a74b575b70cefb23e78a6606e3fe54da90264042
Kiwifuit <mahkiwi123@gmail.com>
2024-09-05 06:51:23 +0800

    feat: `libmcs_version`

--- mcs-ffi/src/lib.rs
+++ mcs-ffi/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::ffi::{c_char, CString};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub extern "C" fn libmcs_version() -> *const c_char {
+    CString::new(format!("mcs backend v{}", env!("CARGO_PKG_VERSION")))
+        .unwrap()
+        .into_raw()
 }
