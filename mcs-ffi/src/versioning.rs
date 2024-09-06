c378ef6a96e2170e1c4528b3380e2ed42afb99ec
Kiwifuit <mahkiwi123@gmail.com>
2024-09-06 19:19:26 +0800

    feat: `modrinth` crate

--- mcs-ffi/src/versioning.rs
+++ mcs-ffi/src/versioning.rs
@@ -3,9 +3,9 @@ use std::ffi::{c_char, CString};
 #[no_mangle]
 pub extern "C" fn libmcs_version() -> *const c_char {
     CString::new(format!(
-        "mcs backend v{} ● mcs {}",
+        "mcs backend v{} ({})",
         env!("CARGO_PKG_VERSION"),
-        env!("GIT_SHA_LONG")
+        env!("GIT_SHA_SHORT")
     ))
     .unwrap()
     .into_raw()
