bea03b5cc5287a997fdce40f42faf1f2f03a5bd6
Kiwifuit <mahkiwi123@gmail.com>
2024-09-07 08:35:02 +0800

    chore: removed `test` function this concludes ffi logging development (for now, as always 😉)

--- mcs-ffi/src/lib.rs
+++ mcs-ffi/src/lib.rs
@@ -1,13 +1,4 @@
-use log::info;
-
 pub mod logging;
 pub mod modrinth;
 pub mod versioning;
 
-#[no_mangle]
-pub extern "C" fn test() {
-    info!("hi");
-    info!("hello");
-    info!("i am burger");
-    info!("yippeeee");
-}
