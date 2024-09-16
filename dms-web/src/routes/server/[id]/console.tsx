bddd74e0d6f4dc9053900dc9faad2140031946f3
Kiwifuit <mahkiwi123@gmail.com>
2024-09-16 18:24:33 +0800

    feat: fixed interval thingymajig ig 🙂

--- dms-web/src/routes/server/[id]/console.tsx
+++ dms-web/src/routes/server/[id]/console.tsx
@@ -25,6 +25,8 @@ export default function Dashboard() {
     autoScroll();
   };
 
+  let dev_intervalid: NodeJS.Timeout;
+
   onMount(() => {
     if (typeof MutationObserver !== undefined) {
       const newContentObserver = new MutationObserver(autoScroll);
@@ -37,7 +39,10 @@ export default function Dashboard() {
         console.error("expected consoleContent to be available on this time!");
       }
 
-      onCleanup(newContentObserver.disconnect);
+      onCleanup(() => {
+        newContentObserver.disconnect();
+        clearInterval(dev_intervalid);
+      });
     }
 
     autoScroll();
@@ -45,7 +50,7 @@ export default function Dashboard() {
 
   // pre-1.0 only
   let counter = 0;
-  setInterval(() => {
+  dev_intervalid = setInterval(() => {
     onNewLog(`new log line ${counter}`);
     counter++;
   }, 1000);
@@ -57,14 +62,14 @@ export default function Dashboard() {
       <div class="col-start-2 mx-4 mt-3 overflow-x-auto">
         <div
           id="log-title"
-          class="dark:bg-dark-dashboard-title flex h-9 content-center p-5"
+          class="flex h-9 content-center p-5 dark:bg-dark-dashboard-title"
         >
           <h1 class="grow self-center text-lg font-bold">Console</h1>
         </div>
         <div
           id="console-content"
           ref={consoleContent}
-          class="dark:bg-dark-dashboard-body h-[500px] overflow-y-auto p-5 font-mono"
+          class="h-[500px] overflow-y-auto p-5 font-mono dark:bg-dark-dashboard-body"
         >
           <For each={logs()} fallback={<p>Fetching logs...</p>}>
             {(log) => <p class="font-mono">{log}</p>}
