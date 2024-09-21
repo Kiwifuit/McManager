4da67ce08c5773987ebc1d25af859debb0d33b58
Kiwifuit <mahkiwi123@gmail.com>
2024-09-21 10:27:11 +0800

    feat: added log uploading

--- dms-web/src/routes/server/[id]/logs.tsx
+++ dms-web/src/routes/server/[id]/logs.tsx
@@ -2,9 +2,21 @@ import { Title } from "@solidjs/meta";
 import { useParams } from "@solidjs/router";
 import { BiSolidCopyAlt } from "solid-icons/bi";
 import { FaSolidUpload } from "solid-icons/fa";
-import { createSignal, For, onCleanup, onMount } from "solid-js";
+import { Accessor, createSignal, For, onCleanup, onMount } from "solid-js";
 import DashboardNavBar from "~/components/ServerDashboard";
 import "./server.css";
+
+type McLogsSucess = {
+  success: true;
+  id: string;
+  url: string;
+  raw: string;
+};
+
+type McLogsError = {
+  success: false;
+  error: string;
+};
 
 export default function Dashboard() {
   const params = useParams();
@@ -61,14 +73,17 @@ export default function Dashboard() {
       <div class="col-start-2 mx-4 mt-3 overflow-x-auto">
         <div
           id="log-title"
-          class="bg-light-dashboard-title flex h-9 content-center p-5 dark:bg-dark-dashboard-title"
+          class="flex h-9 content-center bg-light-dashboard-title p-5 dark:bg-dark-dashboard-title"
         >
           <h1 class="grow self-center text-lg font-bold">Logs</h1>
           <div class="flex gap-3 self-center">
-            <button class="bg-light-dashboard-button rounded-md p-2 dark:bg-dark-dashboard-button">
+            <button class="rounded-md bg-light-dashboard-button p-2 dark:bg-dark-dashboard-button">
               <BiSolidCopyAlt />
             </button>
-            <button class="bg-light-dashboard-button rounded-md p-2 dark:bg-dark-dashboard-button">
+            <button
+              class="rounded-md bg-light-dashboard-button p-2 dark:bg-dark-dashboard-button"
+              onClick={() => uploadLogs(logs)}
+            >
               <FaSolidUpload />
             </button>
           </div>
@@ -76,7 +91,7 @@ export default function Dashboard() {
         <div
           id="log-content"
           ref={consoleContent}
-          class="bg-light-dashboard-body max-h-[500px] overflow-auto text-nowrap p-5 font-mono dark:bg-dark-dashboard-body"
+          class="max-h-[500px] overflow-auto text-nowrap bg-light-dashboard-body p-5 font-mono dark:bg-dark-dashboard-body"
         >
           <table class="table-auto">
             <For each={logs()} fallback={<p>loading...</p>}>
@@ -92,8 +107,44 @@ export default function Dashboard() {
 function LogRow(props: { log_line: string; lineno: number }) {
   return (
     <tr class="whitespace-pre">
-      <td class="w-12 select-none pr-4 text-right font-mono">{props.lineno}</td>
+      <td class="w-12 select-none pr-4 text-right font-mono">
+        {props.lineno + 1}
+      </td>
       <td class="font-mono">{props.log_line}</td>
     </tr>
   );
+}
+
+async function uploadLogs(logs: Accessor<string[]>) {
+  let log_content = logs().join("\n");
+  let log_line_count = logs().length;
+
+  let response = await fetch("https://api.mclo.gs/1/log", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    body: new URLSearchParams({
+      content: log_content,
+    }),
+  });
+
+  // This should only happen if the server's wifi is cut off
+  if (!response.body) {
+    alert("Failed to upload logs");
+    return;
+  }
+
+  // let response_body = new TextDecoder("utf-8").decode();
+  let uploaded_log: McLogsSucess | McLogsError = JSON.parse(
+    await response.text(),
+  );
+
+  if (uploaded_log.success) {
+    let mclogs_url = uploaded_log.url;
+
+    alert(`[OK] Log file: ${mclogs_url}`);
+  } else {
+    alert(`Error while uploading logs: ${uploaded_log.error}`);
+  }
 }
